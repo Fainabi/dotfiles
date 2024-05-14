4c95a53d15d81c0c5534607829b0ab33d0891ea9
Fainabi Arandur <arandurf@gmail.com>
2024-05-14 18:50:43 +0800

    lazyvim: refine emacs keymap

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -7,16 +7,18 @@ local map = LazyVim.safe_keymap_set
 -- emacs keymap
 map("i", "<C-n>", "<down>", { desc = "Move cursor down" })
 map("i", "<C-p>", "<up>", { desc = "Move cursor up" })
-map("i", "<C-e>", "<esc>$a", { desc = "Go to line end" })
-map("i", "<C-a>", "<esc>0i", { desc = "Go to line start" })
+map("i", "<C-e>", "<end>", { desc = "Go to line end" })
+map("i", "<C-a>", "<home>", { desc = "Go to line start" })
 map("i", "<A-f>", "<c-right>", { desc = "Go to next word" })
 map("i", "<A-b>", "<c-left>", { desc = "Go to previous word" })
+map("i", "<C-l>", "<esc>zza", { desc = "Align to center" })
 vim.keymap.set("i", "<C-f>", "<right>", { desc = "Go to next character" })
 vim.keymap.set("i", "<C-b>", "<left>", { desc = "Go to previous character" })
 
 -- helix keymap
-map("n", "gh", "0", { desc = "Go to line start" })
-map("n", "gl", "$", { desc = "Go to line end" })
-map("n", "ge", "G", { desc = "Last line" })
+map({ "n", "x" }, "gh", "0", { desc = "Go to line start" })
+map({ "n", "x" }, "gl", "$", { desc = "Go to line end" })
+map({ "n", "x" }, "ge", "G", { desc = "Last line" })
+
 -- map("n", "<space>k", "K", { desc = "LSP Hover" })
 -- map("n", "<C-c>", "gcc", { desc = "Comment a line" })
